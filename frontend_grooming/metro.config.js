93cf3eeb18c380a26927fd4c25413711415c1577
PetBuddy Dev <petbuddy@example.com>
2026-03-17 14:47:04 +0530

    Migrate frontend_grooming to Expo Go

--- frontend_grooming/metro.config.js
+++ frontend_grooming/metro.config.js
@@ -1,12 +1,5 @@
-const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const { getDefaultConfig } = require('expo/metro-config');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('@react-native/metro-config').MetroConfig}
- */
-const defaultConfig = getDefaultConfig(__dirname);
-const config = {};
+const config = getDefaultConfig(__dirname);
 
-module.exports = mergeConfig(defaultConfig, config);
+module.exports = config;
