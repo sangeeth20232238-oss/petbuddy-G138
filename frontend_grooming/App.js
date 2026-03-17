362f712c1cffd90d850e66ad18e97baa9c6b34d0
sangeeth20232238-oss <enofernando14@gmail.com>
2026-03-17 15:03:35 +0530

    Fresh Expo setup for frontend_grooming with GroomingMain screen

---
+++ frontend_grooming/App.js
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import GroomingMain from './src/screens/GroomingMain';
+
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      <StatusBar style="auto" />
+      <GroomingMain />
+    </SafeAreaProvider>
+  );
+}
