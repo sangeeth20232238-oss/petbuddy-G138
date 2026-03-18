3e7065a96a02761352c962365fa89b7a671c6a4f
sangeeth20232238-oss <enofernando14@gmail.com>
2026-03-18 19:08:38 +0530

    Cleared some errors

--- frontend_grooming/App.js
+++ frontend_grooming/App.js
@@ -1,23 +1,13 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import GroomingMain from './src/screens/GroomingMain';
-import GroomingLocation from './src/screens/GroomingLocation';
-
-const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <NavigationContainer>
-        <StatusBar style="auto" />
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="Grooming" component={GroomingMain} />
-          <Stack.Screen name="GroomingLocation" component={GroomingLocation} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <StatusBar style="auto" />
+      <GroomingMain />
     </SafeAreaProvider>
   );
 }
